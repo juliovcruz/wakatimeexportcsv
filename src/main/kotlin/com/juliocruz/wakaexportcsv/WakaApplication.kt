0b7c3013d39c8dfecffe16d267ee628016a4372a
Julin <juliovcruz0@gmail.com>
2022-03-19 11:15:13 -0300

    chore: adding space

--- src/main/kotlin/com/juliocruz/wakaexportcsv/WakaApplication.kt
+++ src/main/kotlin/com/juliocruz/wakaexportcsv/WakaApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class WakaApplication
 
 fun main(args: Array<String>) {
-	runApplication<WakaApplication>(*args)
+	runApplication<WakaApplication>(*args) 
 }
